064efaa7272291b084d9adffe1d158f82695e2d2
Michael Law 2 <flyinggeese@gmx.com>
2018-11-09 18:16:36 -0500

    creating pokemon from ajax call

--- p.js
+++ p.js
@@ -1,5 +1,6 @@
 let pokedex = document.getElementById('pokedex');
 let searchbtn = document.querySelector('ul > li');
+const ALL_POKEMON = [];
 
 searchbtn.addEventListener('click', searchPokemon);
 // get diff coords by determining client with and perform Math
@@ -33,6 +34,24 @@ function static() {
 function searchPokemon() {
     var q = prompt('Enter Pokemon name or ID number');
     console.log(`You searched for ${q}`);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+           var pokemon = JSON.parse(this.responseText);
+           console.log(pokemon);
+           new Pokemon(pokemon['name'], pokemon['types'][0]['type']['name']);
+        }
+    };
+    xhttp.open("GET", `http://fizal.me/pokeapi/api/v2/id/${q}.json`, true);
+    xhttp.send();
 }
 
 document.addEventListener("click", clickXY);
+
+class Pokemon {
+    constructor(name, element) {
+        this.name = name;
+        this.element = element;
+        ALL_POKEMON.push(this);
+    }
+}
