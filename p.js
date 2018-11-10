c5764b41e4c7f80e9a29f1b9b1130c5f642827f7
Michael Law 2 <flyinggeese@gmx.com>
2018-11-10 17:47:34 -0500

    pokemon are being loaded in with their HD images

--- p.js
+++ p.js
@@ -11,6 +11,7 @@ pwrbtn.addEventListener('click', depressBtn);
 let powerButton = false;
 let atTop = true;
 let atBottom = false;
+let screen1 = document.querySelector('div#top-screen > div:first-child');
 
 document.addEventListener("keydown", function (event) {
     let cursor = document.getElementById('cursor');
@@ -47,7 +48,6 @@ function turnOn() {
 
 function shutdown() {
     powerButton = false;
-    let screen1 = document.querySelector('div#top-screen > div:first-child');
     let screen2 = document.getElementById('screen2');
     let cursor = document.getElementById('cursor');
     let light = document.getElementById('led');
@@ -84,7 +84,27 @@ function searchPokemon() {
         if (this.readyState == 4 && this.status == 200) {
            var pokemon = JSON.parse(this.responseText);
            console.log(pokemon);
+           let existing_img = document.getElementById('pokemon-img');
+           if (existing_img != null) {
+               existing_img.remove();
+           }
            new Pokemon(pokemon['name'], pokemon['types'][0]['type']['name']);
+           screen1.style.backgroundColor = 'gold';
+           screen1.style.backgroundImage = 'linear-gradient(gold, red)';
+           let img = document.createElement('img');
+           if (q.length == 1) {
+               var id = `00${q}`;
+           } else if (q.length == 2) {
+               var id = `0${q}`;
+           } else {
+               var id = `${q}`;
+           }
+           img.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`;
+           img.id = 'pokemon-img';
+           img.classList.add('pokemon-image');
+        //    img.height = '300px';
+        //    img.width = '300px';
+           document.getElementById('top-screen').appendChild(img);
         }
     };
     xhttp.open("GET", `http://fizal.me/pokeapi/api/v2/id/${q}.json`, true);
