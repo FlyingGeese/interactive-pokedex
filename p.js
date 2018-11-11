3298e89964483063521632093fb74db372ea2060
Michael Law 2 <flyinggeese@gmx.com>
2018-11-11 17:51:39 -0500

    next pokemon button works

--- p.js
+++ p.js
@@ -14,6 +14,12 @@ let atBottom = false;
 let screen1 = document.querySelector('div#top-screen > div:first-child');
 let submenu = document.querySelector('#submenu');
 let pokemonLoaded = false;
+let nextArrow = document.getElementById('next-pokemon');
+let prevArrow = document.getElementById('prev-pokemon');
+
+nextArrow.addEventListener('click', getNextPokemon);
+prevArrow.addEventListener('click', getPrevPokemon);
+
 
 document.addEventListener("keydown", function (event) {
     let cursor = document.getElementById('cursor');
@@ -103,7 +109,7 @@ function searchPokemon() {
                let name = document.getElementsByClassName('pokemon-name')[0];
                name.remove();
            }
-           new Pokemon(pokemon['name'], pokemon['types'][0]['type']['name']);
+           new Pokemon(pokemon['id'], pokemon['name'], pokemon['types'][0]['type']['name']);
            screen1.style.backgroundColor = 'lightblue';
            screen1.style.backgroundImage = 'linear-gradient(lightblue, blue)';
            let img = document.createElement('img');
@@ -134,7 +140,8 @@ function searchPokemon() {
            pokemonLoaded = true;
            document.getElementsByClassName('menu')[0].classList.add('hidden');
         } else if (this.readyState == 4 && this.status == 404) {
-            return alert('Pokemon ID out of range');
+            alert('Pokemon ID out of range');
+            return searchPokemon();
         }
     };
     xhttp.open("GET", `http://fizal.me/pokeapi/api/v2/id/${q}.json`, true);
@@ -142,7 +149,8 @@ function searchPokemon() {
 }
 
 class Pokemon {
-    constructor(name, element) {
+    constructor(id, name, element) {
+        this.id = id;
         this.name = name;
         this.element = element;
         ALL_POKEMON.push(this);
@@ -153,6 +161,10 @@ function loadPokemon() {
 
 }
 
+function showSkills() {
+    document.createElement('div');
+}
+
 function  showMenu() {
     let menu = document.querySelectorAll('ul')[0];
     menu.classList.remove('hidden');
@@ -161,4 +173,51 @@ function  showMenu() {
 
 function viewStats() {
     return alert(ALL_POKEMON[ALL_POKEMON.length - 1].name);
+}
+
+function getNextPokemon() {
+    let current_pokemon = ALL_POKEMON[ALL_POKEMON.length - 1].id;
+    console.log(current_pokemon)
+    let nextPokemon = current_pokemon + 1;
+    if (nextPokemon <= 9) {
+        var id = `00${nextPokemon}`;
+    } else if (nextPokemon <= 99) {
+        var id = `0${nextPokemon}`;
+    } else {
+        var id = `${nextPokemon}`;
+    }
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var pokemon = JSON.parse(this.responseText);
+            console.log(pokemon);
+            screen1.style.backgroundColor = 'lightblue';
+            screen1.style.backgroundImage = 'linear-gradient(lightblue, blue)';
+            document.getElementById('pokemon-img').remove();
+            document.getElementsByClassName('pokemon-name')[0].remove();
+            let img = document.createElement('img');
+            let bgImg = document.createElement('img');
+            bgImg.src = 'pokeball.png';
+            bgImg.classList.add('pokedex-info-bg');
+            let h3 = document.createElement('h3');
+            h3.classList.add('pokemon-name');
+            h3.innerHTML = pokemon['name'];
+            img.style.animation = 'fadeIn 0.8s';
+            img.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`;
+            img.id = 'pokemon-img';
+            img.classList.add('pokemon-image');
+            submenu.classList.remove('hidden');
+            document.getElementById('top-screen').appendChild(h3);
+            document.getElementById('top-screen').appendChild(img);
+            document.getElementById('top-screen').appendChild(bgImg);
+            new Pokemon(pokemon['id'], pokemon['name'], pokemon['types'][0]['type']['name']);
+            pokemonLoaded = true;
+        }
+    }
+    xhttp.open("GET", `http://fizal.me/pokeapi/api/v2/id/${nextPokemon}.json`, true);
+    xhttp.send();
+}
+
+function getPrevPokemon() {
+
 }
